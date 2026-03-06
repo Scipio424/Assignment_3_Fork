09298c347e1dd5c3310d5a63ae3622f73fd0d428
Scipio424 <145701065+Scipio424@users.noreply.github.com>
2026-03-06 15:47:36 -0500

    Add tests for multiplication and division scenarios

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -18,12 +18,47 @@ public class SimpleCalculatorTest {
 	}
 	@Test
 	public void testMultiply() {
-    SimpleCalculator calc = new SimpleCalculator();
-    assertEquals(15, calc.multiply(3, 5));
+    	SimpleCalculator calc = new SimpleCalculator();
+    	assertEquals(15, calc.multiply(3, 5));
 	}	
 	@Test
 	public void testDivide() {
-    SimpleCalculator calc = new SimpleCalculator();
-    assertEquals(2, calc.divide(10, 5));
+    	SimpleCalculator calc = new SimpleCalculator();
+    	assertEquals(2, calc.divide(10, 5));
+	}
+	@Test
+	public void testDivideByZero() {
+    	SimpleCalculator calc = new SimpleCalculator();
+    	assertThrows(ArithmeticException.class, () -> calc.divide(10, 0));
+	}
+	@Test
+	public void testMultiplyNegative() {
+   		SimpleCalculator calculator = new SimpleCalculator();
+    	assertEquals(-12, calc.multiply(-3, 4));
+	}
+	@Test
+	public void testMultiplyByZero() {
+    	SimpleCalculator calculator = new SimpleCalculator();
+    	assertEquals(0, calc.multiply(0, 999));
+	}
+	@Test
+	public void testDivideNegative() {
+    	SimpleCalculator calculator = new SimpleCalculator();
+    	assertEquals(-5, calc.divide(-10, 2));
+	}
+	@Test
+	public void testDivideResultZero() {
+    	SimpleCalculator calculator = new SimpleCalculator();
+    	assertEquals(0, calc.divide(1, 5));
+	}
+	@Test
+	public void testMultiplyTwoNegatives() {
+    	SimpleCalculator calculator = new SimpleCalculator();
+    	assertEquals(20, calculator.multiply(-4, -5));
+	}
+	@Test
+	public void testMultiplyLargeNumbers() {
+    	SimpleCalculator calc = new SimpleCalculator();
+    	assertEquals(1000000, calc.multiply(1000, 1000));
 	}
 }
